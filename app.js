2f5ae5bcab4a8b49b51e1b9e4289600f20315dca
Malik Hardy <50418576+Malikhardy23@users.noreply.github.com>
2020-12-29 17:10:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+// SELECT ALL ELEMENTS //
+
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+
+// APP VARIABLES //
+
+let app_data = [],
+    case_list = [],
+    recovered_list = [],
+    deaths_list = [],
+    deaths = [];
+
+// GET USERS COUNTRY CODE //
+
+let country_code = geoplugin_countryCode();
+let user_country;
+country_list.forEach( country => {
+    if(country.code == country_code){
+        user_country = country.name;
+    }
+});
+
+console.log(user_country);
+
+/* API URL AND KEY */
+
+function fetchData(user_country){
+    fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=${user_country}`, {
+        "method": "GET",
+		"headers": {
+			"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
+			"x-rapidapi-key": "7e269ec140msh8a5df9cfc21b4b4p1c1e3ejsn9aba26afc6e0"
+        }
+    })
+    .then( response => {
+        return response.json();
+    })
+    .then( data => {
+        dates = Object.keys(data);
+        dates.forEach(date => {
+            let DATA = data[date];
+
+            app_data.push(DATA);
+            case_list.push(parseInt(DATA.total_cases.replace(/,/g, "")));
+            recovered_list.push(parseInt(DATA.total_reovered.replace(/,/g, "")));
+            deaths_list.push(parseInt(DATA.total_deaths.replace(/,/g, "")));
+        })
+    })
+    .then(() => {
+        updateUI();
+    })
+    .catch(error => {
+        alert(error)
+    })
+}
+
+fetchData(user_country);
+
+
+// Update UI Function //
+
+function updateUI(){
+    updateStats();
+    // axesLinearChart();
+}
+
+function updateStats(){
+    let last_entry = app_data[app_data.length - 1];
+    console.log(last_entry)
+}
